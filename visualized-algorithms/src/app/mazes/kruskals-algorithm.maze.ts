f996d0640482735e13b093f988844f96333a3cc7
Atanas Sarafov <asarafov94@gmail.com>
2020-11-03 23:01:24 -0500

    just another commit to save changes #26

--- visualized-algorithms/src/app/mazes/kruskals-algorithm.maze.ts
+++ visualized-algorithms/src/app/mazes/kruskals-algorithm.maze.ts
@@ -1,11 +1,32 @@
 import {GridService} from '../grid/grid.service';
-import {TileLocationAndState} from '../constants/interfaces';
+import {CoordinateSet, TileLocationAndState} from '../constants/interfaces';
 
 export class KruskalsAlgorithmMaze {
-  constructor(public gridService: GridService) { }
+  private gridStack: TileLocationAndState[];
+  private tileSets;
+  private availableTiles: CoordinateSet[];
+
+constructor(public gridService: GridService) {
+    this.gridStack = [];
+    this.tileSets = [];
+    this.availableTiles = [];
+  }
 
   generateKruskalsAlgorithmMaze(): TileLocationAndState[] {
-    return null;
+    // ALGORITHM START
+    while (true) {
+      // find a random tile that is available (it CAN be part of the gridStack)
+
+      // find a random neighbor of the tile found above that IS NOT part of the gridStack
+      // Requirements for a valid neighbor:
+      // CANNOT: be in this.gridStack
+      // CANNOT: have its diagonal neighbors on the opposite side be in this.gridStack
+      //
+
+      break;
+    }
+
+    return this.gridStack;
   }
 
 }
